4b5fd569b48f3768a76f98a6ac407751f3b95783
_ktakei <lpc.www.com@gmail.com>
2014-08-06 08:25:03 +0900

    各ファイルの修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,22 @@
 var gulp = require('gulp'),
-	$ = require('gulp-load-plugins')();
-	//module.exports = gulp;
-
+	$ = require('gulp-load-plugins')(),
+	sass = require('gulp-ruby-sass');
 
 /*
  * Default Tasks
  */
 // http server
-gulp.task('connect', function() {
-	$.connect.server({
-		root: ['./src'],
-		port: 8000,
+gulp.task('webserver', function() {
+  gulp.src('./src')
+    .pipe($.webserver({
 		livereload: true,
-	});
+		directoryListing: false,
+		fallback: 'index.html'
+    }));
 });
 
-// Reload server
-gulp.task('html', ['hint'], function(){
-	gulp.src('./src')
-		.pipe($.connect.reload());
-});
-// Reload server
-gulp.task('css', ['compass'], function(){
-	gulp.src('./src')
-		.pipe($.connect.reload());
-});
+
+
 
 // Validate HTML
 gulp.task('hint', function() {
@@ -34,31 +26,52 @@ gulp.task('hint', function() {
 		.pipe($.htmlhint.failReporter())
 		.on('error', $.notify.onError(function(error) {
 			return error.message;
-		}));
+		}))
+		.pipe($.connect.reload());
 });
 
-// Compile Compass
-gulp.task('compass', function(){
-	return gulp.src('./scss/*.scss')
-				.pipe($.plumber())
-				.pipe($.compass({
-					//config_file: 'config.rb',
-					comments: false,
-					css: './src/css/',
-					img: './src/images/',
-					sass: './scss/'}))
-				.on('error', $.notify.onError(function(error) {
-					return error.message;
-				}));
+// Compile Sass
+gulp.task('sass', function(){
+	return gulp.src('./scss/{,**/}*.scss')
+		.pipe($.plumber())
+		.pipe(sass({
+			style : 'compact',		// nested, compact, compressed, expanded
+		}))
+		.on('error', function(error) {
+			$.notify().write(error.message)
+		})
+		.pipe(gulp.dest('./src/css'));
 });
+
+
+gulp.task('css', ['sass'], function() {
+	gulp.src('./src/css/{,**/}*.css')
+	.pipe($.pleeease({
+		fallbacks: {
+			autoprefixer: ['last 4 versions'],
+			filters: { 'oldIE': true },
+			rem: false,
+		},
+		optimizers: {
+			minifier: false
+		}
+	}))
+	.pipe($.cssbeautify({
+		indent: '\t',
+		openbrace: 'end-of-line',
+		autosemicolon: false
+	}))
+	.pipe(gulp.dest('./src/css/'))
+	.pipe($.connect.reload());
+});
+
 
 // Watch Files
 gulp.task('watch',function(){
-	gulp.watch(['./src/{,**/}*.html'],['html']);
+	gulp.watch(['./src/{,**/}*.html'],['hint']);
 	gulp.watch(['./scss/{,**/}*.scss'],['css']);
 });
 
 
-gulp.task('default', function() {
-	gulp.run('connect', 'compass', 'watch');
-});
+// Set Defalt
+gulp.task('default', ['webserver', 'sass', 'watch']);
